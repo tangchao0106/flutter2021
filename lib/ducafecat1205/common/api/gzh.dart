e93098bb56ab4fa8f6e1bef7bffcf08df17b3311
tangchao <419704299@qq.com>
2022-01-05 18:46:44 +0800

    feat：2022flutter，每天30分钟练习

--- lib/ducafecat1205/common/api/gzh.dart
+++ lib/ducafecat1205/common/api/gzh.dart
@@ -1,4 +1,6 @@
 // https://wanandroid.com/wxarticle/chapters/json
+import 'dart:convert';
+
 import 'package:logger/logger.dart';
 import 'package:untitled/ducafecat1205/common/entity/wanandroid/gongzhonghao.dart';
 import 'package:untitled/ducafecat1205/common/utils/http.dart';
@@ -7,18 +9,15 @@ import 'package:untitled/ducafecat1205/common/utils/http2.dart';
 class GongZhongHaoApi {
   static var logger = Logger();
 
-  static Future<Gongzhonghao> Futurechapters({
+  static Future<List<Gongzhonghao>> Futurechapters({
     bool refresh = false,
     bool cacheDisk = false,
   }) async {
     var response = await HttpUtil2().get("/wxarticle/chapters/json",
-        // queryParameters: params?.toJson(),
         refresh: refresh,
         cacheDisk: cacheDisk);
     logger.d("公众号=$response");
-    logger.d("公众号=${response.data}");
-
-    return response.data
+    return response['data']
         .map<Gongzhonghao>((item) => Gongzhonghao.fromJson(item))
         .toList;
   }
